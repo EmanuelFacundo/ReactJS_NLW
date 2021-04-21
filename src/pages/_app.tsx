7c15cf976bb0d271f5eec7b5459c4c0afbf7582a
Emanuel <emanuel.facundo14@gmail.com>
2021-04-20 22:23:56 -0300

    Aula dia 2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/global.css';
+import '../styles/global.scss'
+
+import { Header } from '../components/Header'
+import style from '../styles/app.module.scss'
+import { Player } from '../components/Player'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={style.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Deu bom</h1>
+    <div>
+      <h1>Index</h1>
+      
+    </div>
+
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3333/episodes')
+  const data = await res.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
+
 }
